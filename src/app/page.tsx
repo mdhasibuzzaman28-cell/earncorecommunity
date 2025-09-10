99afa47e08b09aff1b2651b4250cf824d139942e
MdHasib01 <md.hasibuzzaman001@gmail.com>
2025-09-10 21:24:53 +0600

    fix dashboard link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <Link href="/dashboard/feed">
+      <Link href="/dashboard/feed/entrepreneurs-hub">
         <Button>Go to Dashboard</Button>
       </Link>
     </div>
